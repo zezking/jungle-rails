9cd89676c30e58f3a84de32c6b1333a40913a730
zezking <zhaoenze001@gmail.com>
2021-03-31 11:38:16 -0700

    feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+      @products = Product.order(id: :desc).all
+      @category = Category.all
   end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-   
     @product = Product.find params[:id]
-
   end
 
 end
