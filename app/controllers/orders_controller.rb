48a10194a648bb46b5a99cef7133bb0b54c8f52f
zezking <zhaoenze001@gmail.com>
2021-03-31 09:28:19 -0700

    add item partials and _empty_cart partial in carts view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
-      empty_cart!
+      enhanced_cart
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
