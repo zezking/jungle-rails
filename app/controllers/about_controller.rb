c7758ceaa8ee18905bdb9beaaa58592dcdf4f0d9
zezking <zhaoenze001@gmail.com>
2021-03-31 16:03:51 -0700

    create categories controllers and add templates files

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,4 @@
 class AboutController < ApplicationController
-  def index
+    def index
+    end
   end
-end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,23 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
   def index
+    @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @product.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  
 end
