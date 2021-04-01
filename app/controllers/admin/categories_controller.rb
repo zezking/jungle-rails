bcc5f4029f305ba82c1cbe608fd5e4bc8e587143
zezking <zhaoenze001@gmail.com>
2021-03-31 17:13:08 -0700

    finish admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -12,12 +12,19 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    if @product.save
-      redirect_to [:admin, :categories], notice: 'Product created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category Created created!'
     else
       render :new
     end
   end
 
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
   
 end
