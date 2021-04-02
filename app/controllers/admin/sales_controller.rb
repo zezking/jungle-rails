10e13264020c6d8bace2190ef6f9145df7fee194
zezking <zhaoenze001@gmail.com>
2021-04-02 15:44:03 -0700

    finish sales status

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SalesController < ApplicationController
     def index
-        
+        @sales=Sale.all
     end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,14 @@
 class Sale < ActiveRecord::Base
+    def finished?
+        ends_on < Date.current
+    end
+
+    def current?
+        starts_on >= Date.current
+    end
+
+    def active?
+        !current? && !finished?
+    end
+    
 end
