8ff36ec9fe22e26d43844ecd55aaf49f7c752be3
zezking <zhaoenze001@gmail.com>
2021-04-02 10:47:54 -0700

    fix params not accessing email using the correct braket notation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
     end
   
     def create
-        user = User.find_by_email(params[:email])
-    
-        if user && user.authenticate(params[:password])
+        user = User.find_by_email(params[:session][:email])
+        if user && user.authenticate(params[:session][:password])
           
           session[:user_id] = user.id
           redirect_to '/'
         else
+          puts 'its here'
           redirect_to '/login'
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,31 +6,25 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
-
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
-
-    get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
   resources :orders, only: [:create, :show]
-
-  
-
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
