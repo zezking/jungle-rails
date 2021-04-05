448178d05ea02c5d8f4b4ed05ef30d3d6c4fb5c2
zezking <zhaoenze001@gmail.com>
2021-04-05 14:33:51 -0700

    finish products test

---
+++ spec/product_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+RSpec.describe Product, type: :model do
+
+    describe 'Validations' do
+
+    
+      
+      it "should be valid with valid attributes" do
+       @product=Product.new(
+                name:"test-product",
+                price:"0",
+                quantity:"1",
+                category:Category.new
+            )
+       expect(@product).to be_valid
+      end         
+      it "should not be valid without a name" do
+        @product=Product.new(
+            name:nil,
+            price:"0",
+            quantity:"1",
+            category:Category.new
+        )
+        expect(@product).to_not be_valid
+      end         
+      it "should not be valid without a price" do
+        @product=Product.new(
+            name:"test-product",
+            price:nil,
+            quantity:"1",
+            category:Category.new
+        )
+        expect(@product).to_not be_valid   
+      end  
+      it "should not be valid without a quantity" do
+        @product=Product.new(
+            name:"test-product",
+            price:"0",
+            quantity:nil,
+            category:Category.new
+        )
+   expect(@product).to_not be_valid
+      end        
+      it "should not be valid without category" do
+        @product=Product.new(
+            name:"test-product",
+            price:"0",
+            quantity:"1",
+            category:nil
+        )
+   expect(@product).to_not be_valid
+      end    
+
+
+    end
+  end
