b0961870c99c1aa266df3f22b637e88af82b160f
zezking <zhaoenze001@gmail.com>
2021-04-06 15:09:42 -0700

    fix

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,27 @@
+
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+RSpec.feature "Add to cart", type: :feature, js: true do
+     # SETUP
+     before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+
+    scenario "Cart gets updated from 0 to 1" do
+      visit root_path
+      expect(page.has_content?('My Cart (0)')).to be(true)
+      find('button.btn-primary', match: :first).click
+      save_screenshot
+      expect(page.has_content?('My Cart (1)')).to be(true)
+    end
+
